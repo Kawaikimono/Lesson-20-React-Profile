1c6d0f1c12200b8ece70d8c455304a65ede3e111
Aly-gator <119552961+Kawaikimono@users.noreply.github.com>
2023-03-05 23:51:42 -0800

    trying to get initial link to work

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Route path="/portfolio" element={<Portfolio image=""/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/resume" element={<Resume/>}/>
-        <Route path="/*/" element={<About/>}/>
+        <Route path="*" element={<About/>}/>
       </Routes>
       </div>
       <Footer/>
